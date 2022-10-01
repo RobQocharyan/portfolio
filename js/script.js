1ebfdd0d935eceea5fe1fe00dd39041c26c78296
RobQocharyan <robqocharyan@mail.ru>
2022-10-01 13:36:13 +0400

    f

--- js/script.js
+++ js/script.js
@@ -107,3 +107,34 @@ $(function () {
     nav.toggleClass("show")
   });
 });
+
+
+// const route = (event)=>{
+//   event = event || window.event;
+//   event.preventDefault();
+//   window.history.pushState({},"",event.target.href)
+//   handleLocation()
+// }
+
+// const routes = {
+//   404:"/templates/404.html",
+//   "/work":"/templates/work.html",
+//   "/blog":"/templates/blog.html",
+//   "/contact":"/templates/contact.html",
+//   "/about":"/templates/about.html",
+//   "/":"index.html"
+// }
+
+
+// const handleLocation = async()=>{
+//   const path = window.location.pathname
+//   const route = routes[path] || routes[404]
+//   const html = await fetch(route).then((data)=>data.text());
+//   document.getElementById("main-page").innerHTML = html
+// }
+
+
+// window.onpopstate = handleLocation
+// window.route = route;
+
+// handleLocation()
